a23c52868c6ba06034beed4972947ca9cdb58859
ArtimirGT <artimir@inbox.ru>
2024-04-02 19:46:23 +0300

    Second Window
    Second Window appearance (Not working)

--- app/instr.py
+++ app/instr.py
@@ -21,21 +21,29 @@ txt_instruction = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit,\
     leo. Nisi porta lorem mollis aliquam ut porttitor leo a diam. Tincidunt \
     vitae semper quis lectus.'
 txt_title = ''
-txt_name = ''
-txt_hintname = "Введите Ф.И.О:"
-txt_hintage = "Введите возраст:"
-txt_test1 = ''
-txt_test2 = ''
-txt_test3 = ''
+txt_name = 'Введите Ф.И.О:'
+txt_hintname = "ФИО"
+txt_hintage = "0"
+txt_test1 = '     Лягте на спину и замерьте пульс за 15 секунд. Нажмите кнопку \
+    "Начать первый тест", чтобы запустить таймер.\
+    Результат запишите в соответствующее поле.'
+txt_test2 = '     Выполните 30 приседаний за 45 секунд. \
+    Для этого нажмите кнопку "Начать делать приседания",\
+    чтобы запустить счетчик приседаний.'
+txt_test3 = f'    Лягте на спину и замерьте пульс сначала за первые \
+    15 секунд минуты, затем за последние 15 секунд.\n\
+    Нажмите кнопку "Начать финальный тест", чтобы запустить таймер.\n\
+    Зеленым обозначены секунды, в течение которых необходимо\n\
+    проводить измерения, черным - минуты без замера пульсаций. Результаты запишите в соответствующие поля.'
 txt_sendresults = ''
 txt_hinttest1 = '0'
 txt_hinttest2 = '0'
 txt_hinttest3 = '0'
-txt_starttest1 = ''
-txt_starttest2 = ''
-txt_starttest3 = ''
+txt_starttest1 = '  Начать первый тест  '
+txt_starttest2 = '  Начать делать приседания    '
+txt_starttest3 = '  Начать делать финальный тест    '
 
-txt_age = ''
+txt_age = 'Полных лет:'
 txt_finalwin = ''
 txt_index = ''
 txt_workheart = ''
--- app/second_win.py
+++ app/second_win.py
@@ -1,11 +1,17 @@
 # напиши здесь код для второго экрана приложения
-from instr.py import *
+from instr import *
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QLabel, QHBoxLayout
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QLabel, QHBoxLayout, QLineEdit
+
+app = QApplication([])
 
 class TestWin(QWidget):
     def __init__(self):
-        pass
+        super().__init__()
+        self.set_appear()
+        self.initUI()
+        self.connects()
+        self.show()
     def set_appear(self):
         self.setWindowTitle('Тест Руфье')
         self.resize(win_width, win_height)
@@ -13,7 +19,26 @@ class TestWin(QWidget):
     def initUI(self):
         self.h_line = QHBoxLayout()
         self.r_line = QVBoxLayout()
-        self.l_line = QHBoxLayout()
-        
+        self.l_line = QVBoxLayout()
+        self.l_line.addWidget(QLabel(txt_name), alignment=Qt.AlignLeft)
+        self.l_line.addWidget(QLineEdit(txt_hintname), alignment=Qt.AlignLeft)
+        self.l_line.addWidget(QLabel(txt_age), alignment=Qt.AlignLeft)
+        self.l_line.addWidget(QLineEdit(txt_hintage), alignment=Qt.AlignLeft)
+        self.l_line.addWidget(QLabel(txt_test1), alignment=Qt.AlignLeft)
+        self.l_line.addWidget(QPushButton(txt_starttest1), alignment=Qt.AlignLeft)
+        self.l_line.addWidget(QLineEdit(txt_hinttest1), alignment=Qt.AlignLeft)
+        self.l_line.addWidget(QLabel(txt_test2), alignment=Qt.AlignLeft)
+        self.l_line.addWidget(QPushButton(txt_starttest2), alignment=Qt.AlignLeft)
+        self.l_line.addWidget(QLabel(txt_test3), alignment=Qt.AlignLeft)
+        self.l_line.addWidget(QPushButton(txt_starttest3), alignment=Qt.AlignLeft)
+        self.l_line.addWidget(QLineEdit(txt_hinttest2), alignment=Qt.AlignLeft)
+        self.l_line.addWidget(QLineEdit(txt_hinttest3), alignment=Qt.AlignLeft)
+        self.r_line.addWidget(QLabel('<b>ЗДЕСЬ БУДЕТ ТАЙМЕР<b>'), alignment=Qt.AlignCenter)
+        self.h_line.addLayout(self.l_line)
+        self.h_line.addLayout(self.r_line)
+        self.setLayout(self.h_line)
     def connects(self):
         pass
+
+test = TestWin()
+app.exec_()
