c93bd667be38593580911b244dec7a7ca275b6bd
ArtimirGT <artimir@inbox.ru>
2024-04-02 18:57:06 +0300

    init

---
+++ app/final_win.py
@@ -0,0 +1,3 @@
+# напиши здесь код третьего экрана приложения
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QLabel, QHBoxLayout
---
+++ app/instr.py
@@ -0,0 +1,41 @@
+
+win_x, win_y = 200, 100
+win_width, win_height = 1000, 600
+
+txt_hello = 'Добро пожаловать! Lorem ipsum aboba safajn sfad'
+txt_next = ''
+txt_instruction = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit,\
+    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. \
+    Volutpat lacus laoreet non curabitur gravida arcu ac tortor dignissim. \
+    Cursus risus at ultrices mi tempus imperdiet nulla. Etiam non quam lacus\
+    suspendisse. Nunc sed augue lacus viverra vitae congue eu consequat. \
+    Magnis dis parturient montes nascetur. Faucibus interdum posuere lorem \
+    ipsum dolor sit amet consectetur. Vitae tempus quam pellentesque nec nam. \
+    Proin fermentum leo vel orci. Sed id semper risus in hendrerit gravida \
+    rutrum quisque. Arcu risus quis varius quam. Cursus vitae congue mauris \
+    rhoncus aenean vel elit scelerisque. Laoreet non curabitur gravida arcu ac \
+    tortor dignissim convallis. Quis vel eros donec ac odio tempor orci. \
+    Rhoncus mattis rhoncus urna neque viverra justo nec ultrices dui. \
+    Commodo quis imperdiet massa tincidunt nunc pulvinar. Luctus accumsan \
+    tortor posuere ac. Nibh ipsum consequat nisl vel pretium lectus quam id \
+    leo. Nisi porta lorem mollis aliquam ut porttitor leo a diam. Tincidunt \
+    vitae semper quis lectus.'
+txt_title = ''
+txt_name = ''
+txt_hintname = "Введите Ф.И.О:"
+txt_hintage = "Введите возраст:"
+txt_test1 = ''
+txt_test2 = ''
+txt_test3 = ''
+txt_sendresults = ''
+txt_hinttest1 = '0'
+txt_hinttest2 = '0'
+txt_hinttest3 = '0'
+txt_starttest1 = ''
+txt_starttest2 = ''
+txt_starttest3 = ''
+
+txt_age = ''
+txt_finalwin = ''
+txt_index = ''
+txt_workheart = ''
