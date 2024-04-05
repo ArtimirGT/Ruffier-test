514ecf8be7258a5c64de5f4104d3990367ca90a0
ArtimirGT <artimir@inbox.ru>
2024-04-05 15:15:48 +0300

    Second Window Timer
    timer is now can be activated with starttest buttons

--- app/final_win.py
+++ app/final_win.py
@@ -1,3 +1,21 @@
 # напиши здесь код третьего экрана приложения
+from instr import *
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QLabel, QHBoxLayout
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QLabel, QHBoxLayout, QLineEdit
+
+class finalWin(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.set_appear()
+        self.initUI()
+        self.show()
+    def set_appear(self):
+        self.setWindowTitle(txt_title)
+        self.resize(win_width, win_height)
+        self.move(win_x, win_y)
+    def initUI(self):
+        self.main_layout = QVBoxLayout()
+        self.main_layout.addWidget(QLabel(txt_index), alignment= Qt.AlignCenter)
+        self.main_layout.addWidget(QLabel(txt_title), alignment= Qt.AlignCenter)
+        self.setLayout(self.main_layout)
+    
--- app/second_win.py
+++ app/second_win.py
@@ -1,6 +1,8 @@
 # напиши здесь код для второго экрана приложения
+from final_win import *
 from instr import *
-from PyQt5.QtCore import Qt
+from PyQt5 import QtGui
+from PyQt5.QtCore import Qt, QTimer, QTime
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QLabel, QHBoxLayout, QLineEdit
 
 app = QApplication([])
@@ -12,11 +14,34 @@ class TestWin(QWidget):
         self.initUI()
         self.connects()
         self.show()
+    def timer_event(self):
+        self.time = self.time.addSecs(1)
+        if self.time.toString("hh:mm:ss") == f'00:00:{self.endtime}':
+            self.timer.stop()
+            self.time = QTime(0, 0, 0)
+        self.timer_text.setText(self.time.toString("<b>hh:mm:ss<b>"))
+
+    def test1_start(self):
+        self.time = QTime(0, 0, 0)
+        self.timer.start(1000)
+        self.endtime = '15'
+    def test2_start(self):
+        self.time = QTime(0, 0, 0)
+        self.timer.start(1000)
+        self.endtime = '45'
+    def test3_start(self):
+        self.time = QTime(0, 0, 0)
+        self.timer.start(1000)
+        self.endtime = '15'
+
     def set_appear(self):
-        self.setWindowTitle('Тест Руфье')
+        self.setWindowTitle(txt_title)
         self.resize(win_width, win_height)
         self.move(win_x, win_y)
     def initUI(self):
+        self.timer = QTimer()
+        self.time = QTime(0, 0, 0)
+        self.timer.timeout.connect(self.timer_event)
         self.h_line = QHBoxLayout()
         self.r_line = QVBoxLayout()
         self.l_line = QVBoxLayout()
@@ -25,20 +50,33 @@ class TestWin(QWidget):
         self.l_line.addWidget(QLabel(txt_age), alignment=Qt.AlignLeft)
         self.l_line.addWidget(QLineEdit(txt_hintage), alignment=Qt.AlignLeft)
         self.l_line.addWidget(QLabel(txt_test1), alignment=Qt.AlignLeft)
-        self.l_line.addWidget(QPushButton(txt_starttest1), alignment=Qt.AlignLeft)
+        self.test1_but = QPushButton(txt_starttest1)
+        self.l_line.addWidget(self.test1_but, alignment=Qt.AlignLeft)
         self.l_line.addWidget(QLineEdit(txt_hinttest1), alignment=Qt.AlignLeft)
         self.l_line.addWidget(QLabel(txt_test2), alignment=Qt.AlignLeft)
-        self.l_line.addWidget(QPushButton(txt_starttest2), alignment=Qt.AlignLeft)
+        self.test2_but = QPushButton(txt_starttest2)
+        self.l_line.addWidget(self.test2_but, alignment=Qt.AlignLeft)
         self.l_line.addWidget(QLabel(txt_test3), alignment=Qt.AlignLeft)
-        self.l_line.addWidget(QPushButton(txt_starttest3), alignment=Qt.AlignLeft)
+        self.test3_but = QPushButton(txt_starttest3)
+        self.l_line.addWidget(self.test3_but, alignment=Qt.AlignLeft)
         self.l_line.addWidget(QLineEdit(txt_hinttest2), alignment=Qt.AlignLeft)
         self.l_line.addWidget(QLineEdit(txt_hinttest3), alignment=Qt.AlignLeft)
-        self.r_line.addWidget(QLabel('<b>ЗДЕСЬ БУДЕТ ТАЙМЕР<b>'), alignment=Qt.AlignCenter)
+        self.but_sendresults = QPushButton(txt_sendresults)
+        self.l_line.addWidget(self.but_sendresults, alignment=Qt.AlignCenter)
+        self.timer_text = QLabel('<b>00:00:00<b>')
+        self.timer_text.setFont(QtGui.QFont('Times', 30))
+        self.r_line.addWidget(self.timer_text, alignment=Qt.AlignCenter)
         self.h_line.addLayout(self.l_line)
         self.h_line.addLayout(self.r_line)
         self.setLayout(self.h_line)
     def connects(self):
-        pass
-
+        self.test1_but.clicked.connect(self.test1_start)
+        self.test2_but.clicked.connect(self.test2_start)
+        self.test3_but.clicked.connect(self.test3_start)
+        self.but_sendresults.clicked.connect(self.next)
+    def next(self):
+        self.hide()
+        self.final = finalWin()
+        
 test = TestWin()
 app.exec_()
