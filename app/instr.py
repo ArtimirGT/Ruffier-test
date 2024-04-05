5f1397b04efc59289f392d9b186c3fd07d0b826b
ArtimirGT <artimir@inbox.ru>
2024-04-05 18:31:36 +0300

    Text tweaks

--- app/instr.py
+++ app/instr.py
@@ -3,23 +3,15 @@ win_x, win_y = 200, 100
 win_width, win_height = 1000, 600
 
 txt_hello = 'Добро пожаловать! Lorem ipsum aboba safajn sfad'
-txt_next = ''
-txt_instruction = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit,\
-    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. \
-    Volutpat lacus laoreet non curabitur gravida arcu ac tortor dignissim. \
-    Cursus risus at ultrices mi tempus imperdiet nulla. Etiam non quam lacus\
-    suspendisse. Nunc sed augue lacus viverra vitae congue eu consequat. \
-    Magnis dis parturient montes nascetur. Faucibus interdum posuere lorem \
-    ipsum dolor sit amet consectetur. Vitae tempus quam pellentesque nec nam. \
-    Proin fermentum leo vel orci. Sed id semper risus in hendrerit gravida \
-    rutrum quisque. Arcu risus quis varius quam. Cursus vitae congue mauris \
-    rhoncus aenean vel elit scelerisque. Laoreet non curabitur gravida arcu ac \
-    tortor dignissim convallis. Quis vel eros donec ac odio tempor orci. \
-    Rhoncus mattis rhoncus urna neque viverra justo nec ultrices dui. \
-    Commodo quis imperdiet massa tincidunt nunc pulvinar. Luctus accumsan \
-    tortor posuere ac. Nibh ipsum consequat nisl vel pretium lectus quam id \
-    leo. Nisi porta lorem mollis aliquam ut porttitor leo a diam. Tincidunt \
-    vitae semper quis lectus.'
+txt_next = 'Начать'
+txt_instruction = 'Данное приложение позволит вам с помощью теста Руфье провести первичную диагностику вашего здоровья.\n\
+Проба Руфье представляет собой нагрузочный комплекс, предназначенный для оценки работоспособности сердца при физической нагрузке.\n\
+У испытуемого, находящегося в положении лежа на спине в течение 5 мин, определяют частоту пульса за 15 секунд;\n\
+затем в течение 45 секунд испытуемый выполняет 30 приседаний.\n\
+После окончания нагрузки испытуемый ложится, и у него вновь подсчитывается число пульсаций за первые 15 секунд,\n\
+а потом - за последние 15 секунд первой минуты периода восстановления.\n\
+Важно! Если в процессе проведения испытания вы почувствуете себя плохо (появится головокружение, шум в\n\
+ушах, сильная одышка и др.), то тест необходимо прервать и обратиться к врачу.'
 txt_title = 'Здоровье'
 txt_name = 'Введите Ф.И.О:'
 txt_hintname = "ФИО"
